fd226ae2b4402e650c8c133001d4bfa759559bf2
Paweł Bihun <pawelbihun@gmail.com>
2021-03-20 13:46:56 +0100

    app v1.0 complited

--- src/js/index.js
+++ src/js/index.js
@@ -10,17 +10,27 @@ registerSW();
 const counter = document.querySelector(".glass__counter--js");
 const add = document.querySelector(".button--add--js");
 const remove = document.querySelector(".button--remove--js");
-
+const key = new Date().toLocaleString().slice(0, 10);
 let currentAmount = 0;
+
+const localStorageValue = localStorage.getItem(key);
+if (localStorageValue) {
+  currentAmount = localStorageValue;
+} else {
+  localStorage.setItem(key , 0);
+}
+counter.innerHTML = currentAmount;
+
 
 add.addEventListener("click", () => {
   currentAmount++;
   counter.innerHTML = currentAmount;
+  localStorage.setItem(key, currentAmount);
 });
-
 remove.addEventListener("click", () => {
   if (currentAmount > 0) {
     currentAmount--;
     counter.innerHTML = currentAmount;
+    localStorage.setItem(key, currentAmount);
   }
 });
